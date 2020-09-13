8382a9293be3475d9357b7deb6adcc4b3ac73158
YifanYang2000 <yifan.yang@hotmail.ca>
2020-09-13 16:34:23 -0400

    front page done

---
+++ extra.js
@@ -0,0 +1,43 @@
+/* progression */
+window.onscroll = function() {progression()};
+
+function progression() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.height = scrolled + "%";
+}
+
+
+/* Typed */
+var typed = new Typed('.animate', {
+    strings: [
+        "DEVELOPER.^2000",
+        "CREATOR.^2000",
+        "ARTIST.^2000",
+        "INNOVATOR.^2000"
+    ],
+    typeSpeed: 200,
+    backSpeed: 100,
+    loop: true
+});
+
+/* Audio */
+var yourAudio = document.getElementById('yourAudio'),
+    ctrl = document.getElementById('audioControl');
+
+ctrl.onclick = function () {
+
+    // Update the Button
+    var pause = ctrl.innerHTML === 'MUTE';
+    ctrl.innerHTML = pause ? 'PLAY.' : 'MUTE';
+
+    // Update the Audio
+    var method = pause ? 'pause' : 'play';
+    yourAudio[method]();
+
+    // Prevent Default Action
+    return false;
+};
+
+
